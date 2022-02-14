9eda388fb3daa3f281bdeb446607fa05a2d91025
Shahid Saddique <msaddique@Powersoft19.com>
2022-02-14 13:18:07 +0500

    Initial olx scrapping added.

--- Selenium/WebScraping/WebScraping/WebScraping.py
+++ Selenium/WebScraping/WebScraping/WebScraping.py
@@ -2,28 +2,41 @@
 # Tutorial link: https://realpython.com/beautiful-soup-web-scraper-python/
 import requests
 from bs4 import BeautifulSoup
+from lxml import etree
 
-URL = "https://realpython.github.io/fake-jobs/"
+URL = "https://www.olx.com.pk/lahore_g4060673"
 page = requests.get(URL)
 
 print(page.text)
 
 soup = BeautifulSoup(page.content, "html.parser")
 
-results = soup.find(id="ResultsContainer")
+#results = soup.find(id="ResultsContainer")
+results = soup.find(class_="_7e3920c1")
 print(results.prettify())
 
-job_elements = results.find_all("div", class_="card-content")
+job_elements = results.find_all("div", class_="a52608cc")
 
 print("=========================================================")
 
+status = "No element found"
+
 for job_element in job_elements:
-    title_element = job_element.find("h2", class_="title")
-    finalTitle = title_element.text
-    company_element = job_element.find("h3", class_="company")
-    location_element = job_element.find("p", class_="location")
-    print(title_element.text.strip())
-    print(company_element.text.strip())
-    print(location_element.text.strip())
+    title_element = job_element.find("div", class_="a5112ca8")
+    company_element = job_element.find("div", class_="_52497c97")
+    location_element = job_element.find("span", class_="_424bf2a8")
+    try:
+        print(title_element.text.strip())
+    except :
+        print(status)
+    try:
+        print(company_element.text.strip())
+    except :
+        print(status)
+    try:
+        print(location_element.text.strip())
+    except :
+        print(status)
+    
     print("--------Next---------")
 print("=========================================================")
