c8e7b6ceb3b5a6d6adc7b9f59df22449e5368c21
Shahid Saddique <shahidali6@gmail.com>
2022-03-01 10:05:36 +0500

    1. initiate_driver_get_cookies called in main file.

--- Selenium/WebScraping/WebScraping/time_logger_digital_attendance_system.py
+++ Selenium/WebScraping/WebScraping/time_logger_digital_attendance_system.py
@@ -6,15 +6,30 @@ csv_object = csv_operations()
 listss1 = ["apple", "banana"]
 
 list_csv = csv_object.write_csvfile("myfile", listss1)
-list_csv_read = csv_object.read_csvfile(r"C:\Users\shahid\source\repos\shahidali6\python\Selenium\WebScraping\WebScraping\olx.csv")
 
 print("this si sdfs")
 
 driver_object = driver_chrome()
 
-#baseURL = "https://www.airliftexpress.com/"
-baseURL = "https://www.google.com/"
+baseURL = "https://www.airliftexpress.com/"
+#baseURL = "https://www.google.com/"
+
+
+#Fields
+countryXpath = "//*[@id=\"cdk-overlay-0\"]/nz-modal-container/div/div/div/ecp-update-delivery-location/ecp-lazy-google-map-wrapper/div/div[2]/form/div/div/div[1]/nz-form-item/nz-form-control/div/div/nz-select/nz-select-top-control/nz-select-search/input"
+countryText = "Pakistan"
+cityxPath = "//*[@id=\"cdk-overlay-0\"]/nz-modal-container/div/div/div/ecp-update-delivery-location/ecp-lazy-google-map-wrapper/div/div[2]/form/div/div/div[2]/nz-form-item/nz-form-control/div/div/nz-select/nz-select-top-control/nz-select-search/input"
+cityText = "Sialkot"
+areaxPath = "//*[@id=\"cdk-overlay-0\"]/nz-modal-container/div/div/div/ecp-update-delivery-location/ecp-lazy-google-map-wrapper/div/div[2]/form/div/div[2]/div[1]/div/nz-form-item/nz-form-control/div/div/nz-select/nz-select-top-control/nz-select-search/input"
+areaText = "Neka Pura, Sialkot"
+nekaPuraxpath = "//*[@id=\"cdk-overlay-5\"]/nz-option-container/div/cdk-virtual-scroll-viewport/div[1]/nz-option-item[1]/div/div/strong"
+continueButtonxPath = "//*[@id=\"cdk-overlay-0\"]/nz-modal-container/div/div/div/ecp-update-delivery-location/ecp-lazy-google-map-wrapper/div/div[2]/form/div/div[2]/div[3]/button"
+
+driver_object.initiate_driver_get_cookies(baseURL, countryXpath, countryText, cityxPath, cityText, areaxPath, areaText,nekaPuraxpath, continueButtonxPath)
 
 driver_object.initiate_driver(baseURL)
+countryXpath = "//*[@id=\"cdk-overlay-0\"]/nz-modal-container/div/div/div/ecp-update-delivery-location/ecp-lazy-google-map-wrapper/div/div[2]/form/div/div/div[1]/nz-form-item/nz-form-control/div/div/nz-select/nz-select-top-control/nz-select-search/input"
+
+driver_object.find_element_by_xpath_and_send_string_key(countryXpath, "Pakistan")
 
 del driver_object
