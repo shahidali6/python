05e7c9bf3e29cf84bdc40c977e3c0dde24e28f70
Shahid Saddique <shahidali6@gmail.com>
2022-03-03 09:20:48 +0500

    trying to add selenium session into request.

--- Selenium/WebScraping/WebScraping/selenium_operations/driver_chrome.py
+++ Selenium/WebScraping/WebScraping/selenium_operations/driver_chrome.py
@@ -1,14 +1,17 @@
 import time
 from selenium import webdriver
+from selenium.webdriver.common import keys
 from selenium.webdriver.common.keys import Keys
 
 # import namespaces to show error message
 # from tkinter import *
 from tkinter import messagebox
 
+import requests
+from bs4 import BeautifulSoup
 
 class driver_chrome:
-    baseURL = "https://www.airliftexpress.com/"
+    baseURL = "https://www.airliftexpress.com"
     main_driver = ""
 
     def initiate_driver(self, base_url):
@@ -82,6 +85,64 @@ class driver_chrome:
             time.sleep(5)
             getElement = driver.find_element_by_xpath(continueButtonxPath).click()
             driver.implicitly_wait(delay+1)
+
+            s = requests.Session()
+            # Set correct user agent
+            selenium_user_agent = driver.execute_script("return navigator.userAgent;")
+            s.headers.update({"user-agent": selenium_user_agent})
+
+            cookkkk = driver.get_cookies()
+            
+            f = open("receivedCookies.txt", "w")
+
+            for x in cookkkk:
+                f.write(str(x))
+            f.close()
+
+            cooooookies = {}
+            #for cookie in driver.get_cookies():
+            #    s.cookies.set(cookie['name'], cookie['value'], domain=cookie['domain'])
+            time.sleep(5) #This is the fix
+
+            cookiesItesm = driver.get_cookies()
+
+            requestCookiesList = []
+
+            for cookie in cookiesItesm:
+                one_cookie = {}
+
+                for key, value in cookie.items():
+                    #test =  cookie[cookie['name']] = cookie['value']
+                    #one_cookie[cookie['name']] = cookie['value']
+                    one_cookie.update([key] = value)
+
+                requestCookiesList.append(one_cookie)
+
+
+            f = open("requestCookies.txt", "w")
+
+            for x in s.cookies:
+                f.write(str(x))
+            f.close()
+
+            URL = baseURL+ "/product-category/promotions"
+            page = s.get(URL, cooooookies)
+
+            soup = BeautifulSoup(page.content, "html.parser")
+
+            f = open("demofile3.html", "w")
+
+
+            f.write(str(page.content))
+            f.close()
+
+            #results = soup.find(id="ResultsContainer")
+            #results = soup.find(class_="scrollable-content")
+            results = soup.find("div",{"class":"scrollable-content"})
+            print(results)
+
+            allElements = results.find_all("article", class_="_7e3920c1")
+
             print("tttttt")
 
         except Exception as ex:
