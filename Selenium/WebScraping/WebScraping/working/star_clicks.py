e300effe0036c8a99761a292fd4070d99c99629b
Shahid Saddique <shahidali6@gmail.com>
2022-03-16 02:17:14 +0500

    write and read the list of proxies functionality added.

--- Selenium/WebScraping/WebScraping/working/star_clicks.py
+++ Selenium/WebScraping/WebScraping/working/star_clicks.py
@@ -1,1 +1,12 @@
+from common.beautifulsoup_operations import beautifulsoup_operations
+from common.file.csv_operations import csv_operations
 
+bs = beautifulsoup_operations()
+list_of_proxyies = bs.read_proxies_sslproxies_org()
+
+csv = csv_operations()
+csv.write_list_to_txt('ListofProxy', list_of_proxyies)
+
+listofips = csv.read_txt_to_list('ListofProxy')
+
+print("file written!")
