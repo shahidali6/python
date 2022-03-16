9fae656149124c7b837f16622c3a111392dfdead
Shahid Saddique <shahidali6@gmail.com>
2022-03-16 10:26:04 +0500

    use fake user agent library instead of hard coded.

--- Selenium/WebScraping/WebScraping/working/useragent.py
+++ Selenium/WebScraping/WebScraping/working/useragent.py
@@ -1,23 +1,35 @@
 import requests
 import random
-user_agent_list = [
-    'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_5) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/13.1.1 Safari/605.1.15',
-    'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:77.0) Gecko/20100101 Firefox/77.0',
-    'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.97 Safari/537.36',
-    'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.15; rv:77.0) Gecko/20100101 Firefox/77.0',
-    'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.97 Safari/537.36',
-]
+from fake_useragent import UserAgent
 
-# Best link to get User agents https://www.whatismybrowser.com/guides/the-latest-user-agent/
-url = 'https://httpbin.org/headers'
-for i in range(1, 4):
-    # Pick a random user agent
-    user_agent = random.choice(user_agent_list)
-    # Set the headers
-    headers = {'User-Agent': user_agent}
-    # Make the request
-    response = requests.get(url, headers=headers)
-    print("Request #%d\nUser-Agent Sent:%s\n\nHeaders Recevied by HTTPBin:" %
-          (i, user_agent))
-    print(response.json())
-    print("-------------------")
+class user_agent:
+    def random_user_agent(self):
+        try:
+            # Fake user agent tested.
+            # https://stackoverflow.com/questions/27652543/how-to-use-python-requests-to-fake-a-browser-visit-a-k-a-and-generate-user-agent
+            fake_user_agent = UserAgent()
+            return fake_user_agent.random
+        except Exception as ex:
+            messagebox.showerror("Exception Message", "Exception: " + str(ex)) 
+
+#user_agent_list = [
+#    'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_5) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/13.1.1 Safari/605.1.15',
+#    'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:77.0) Gecko/20100101 Firefox/77.0',
+#    'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.97 Safari/537.36',
+#    'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.15; rv:77.0) Gecko/20100101 Firefox/77.0',
+#    'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.97 Safari/537.36',
+#]
+
+## Best link to get User agents https://www.whatismybrowser.com/guides/the-latest-user-agent/
+#url = 'https://httpbin.org/headers'
+#for i in range(1, 4):
+#    # Pick a random user agent
+#    user_agent = random.choice(user_agent_list)
+#    # Set the headers
+#    headers = {'User-Agent': user_agent}
+#    # Make the request
+#    response = requests.get(url, headers=headers)
+#    print("Request #%d\nUser-Agent Sent:%s\n\nHeaders Recevied by HTTPBin:" %
+#          (i, user_agent))
+#    print(response.json())
+#    print("-------------------")
