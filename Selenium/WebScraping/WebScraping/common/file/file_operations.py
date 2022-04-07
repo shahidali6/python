91bb9ecb990c0fcd07955cd37ead78d399bd8b29
Shahid Saddique <shahidali6@gmail.com>
2022-04-07 14:22:15 +0500

    1. fileoperation class added. 2. three basic funtion added to get the file list

--- Selenium/WebScraping/WebScraping/common/file/file_operations.py
+++ Selenium/WebScraping/WebScraping/common/file/file_operations.py
@@ -1,1 +1,29 @@
+#Import Operating system library
+import os
+from os import listdir
+from os.path import isfile, join
 
+cwd = os.getcwd()
+
+class fileOperations():
+    #STatic mathods of files and directories
+    @staticmethod
+    def ListOfFileAndDirectoriesSpecificPath(path):
+        files_directories = os.listdir(path)
+        return files_directories
+
+    def ListOfFileAndDirectoriesCurrentDirectory():
+        files_directories = os.listdir(os.getcwd())
+        return files_directories
+
+    def ListFileCurrentDirectory(fileextension):
+        files_directories = os.listdir(os.getcwd())
+        list_files = []
+        for filename in files_directories:
+            if filename.endswith('.'+fileextension):
+                list_files.append(filename)            
+        return list_files
+
+    def List_of_all_files(path):
+        all_files = os.listdir()
+        return all_files
