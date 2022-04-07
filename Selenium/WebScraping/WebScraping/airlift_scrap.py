f320b7e0510d367b86adf05ca0c95d04337af60d
Shahid Saddique <shahidali6@gmail.com>
2022-04-08 03:45:06 +0500

    loop added for cookies file name condition added against lahore file name of cookie catogry name added into SQL method.

--- Selenium/WebScraping/WebScraping/airlift_scrap.py
+++ Selenium/WebScraping/WebScraping/airlift_scrap.py
@@ -2,6 +2,7 @@
 # Tutorial link: https://realpython.com/beautiful-soup-web-scraper-python/
 # XPATH tutorial reference added https://www.geeksforgeeks.org/how-to-use-xpath-with-beautifulsoup/
 # Very useful tutorial link https://www.dataquest.io/blog/web-scraping-beautifulsoup/
+#https://stackoverflow.com/questions/55689701/how-to-use-tor-with-chrome-browser-through-selenium
 from typing import final
 import requests
 from bs4 import BeautifulSoup
@@ -33,7 +34,7 @@ def insert_data_into_airlift_table(list_to_insert_database):
         )
         mycursor = mydb.cursor()
 
-        sql = "INSERT INTO airlift (name,price,image,link,coin,orignal_price,discount_percentage,product_avalible,location) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s)"
+        sql = "INSERT INTO airlift (name,category,price,image,link,coin,orignal_price,discount_percentage,product_avalible,location) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s)"
         mycursor.execute(sql, list_to_insert_database)
         mydb.commit()
 
@@ -46,7 +47,7 @@ def insert_data_into_airlift_table(list_to_insert_database):
         )
         mycursor = mydb.cursor()
 
-        sql = "INSERT INTO airlift (name,price,image,link,coin,orignal_price,discount_percentage,product_avalible,location) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s)"
+        sql = "INSERT INTO airlift (name,category,price,image,link,coin,orignal_price,discount_percentage,product_avalible,location) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s)"
         mycursor.execute(sql, list_to_insert_database)
         mydb.commit()
 
@@ -95,171 +96,162 @@ def unique_listoflist(list_data):
     return unique_data
 
 #listOfAllFiles = fileOperations.ListOfFileAndDirectoriesCurrentDirectory()
-#cookiesfiles = fileOperations.ListFileCurrentDirectory('pkl')
+cookiesfiles = fileOperations.ListFileCurrentDirectory('pkl')
 
-#Lahore: "lahore_g4060673"
-#baseURL = "https://www.olx.com.pk"
-#URL = baseURL+ "/lahore_g4060673"
-base_url = "https://www.airliftexpress.com"
+for cookie in cookiesfiles:
+    #Lahore: "lahore_g4060673"
+    #baseURL = "https://www.olx.com.pk"
+    #URL = baseURL+ "/lahore_g4060673"
+    base_url = "https://www.airliftexpress.com"
 
-driver = webdriver.Chrome()
-driver.maximize_window()
-driver.get(base_url)
-driver.execute_script("document.body.style.zoom='70%'")
-time.sleep(5)
-
-# load saved cookies
-cookies = pickle.load(open("airlift_nekapura_sialkot.pkl", "rb"))
-for cookie in cookies:
-    driver.add_cookie(cookie)
-file_operat = csv_operations()
-driver.get(base_url)
-time.sleep(5)
-
-soup = BeautifulSoup(driver.page_source, "html.parser")
-
-#all_links = soup.select_one('ecp-app-side-panel aside ecp-category nav ul')
-                #class_='table table-striped table-bordered')
-all_links_raw = soup.select('ecp-category nav ul li a')
-all_links = []
-all_link_categories = []
-loop_counter = 1
-for link in all_links_raw:
-    full_link = base_url + link.attrs['href']
-    category_name = link.text
-    all_links.append(full_link)
-    all_link_categories.append(category_name)
-    print(full_link)
-    loop_counter += 1
-
-# save cookies
-#pickle.dump( driver.get_cookies() , open("cookies.pkl","wb"))
-
-# driver.get('https://www.airliftexpress.com/product-category/promotions?page=50')
-#driver.get('https://www.airliftexpress.com/product-category/promotions')
-all_item_list = []
-start_time=time.time()
-
-for link in all_links:
-    driver.get(link)
-    driver.execute_script("document.body.style.zoom='zoom 75%'")
+    driver = webdriver.Chrome()
+    driver.maximize_window()
+    driver.get(base_url)
+    driver.execute_script("document.body.style.zoom='70%'")
     time.sleep(5)
-    db_operation = database_operations()
-    SCROLL_PAUSE_TIME = 1
-    finalbreak = 0
-    last_product_1 = ''
-    last_product_2 = ''
-    external_list = []
-    while True:
-        soup = BeautifulSoup(driver.page_source, 'html.parser')
 
-        location = soup.find('span', class_='dlb-location').text
+    # load saved cookies
+    #cookies = pickle.load(open("airlift_nekapura_sialkot.pkl", "rb"))
+    cookies = pickle.load(open(cookie, "rb"))
+    for cookie in cookies:
+        driver.add_cookie(cookie)
+    file_operat = csv_operations()
+    if 'lahore' in cookie:
+        base_url = 'https://www.airliftexpress.com/product-category/grocery'
+    driver.get(base_url)
+    time.sleep(5)
 
-        all_products = soup.findAll('div', class_='product-card ng-star-inserted')
-        if len(all_products) == 0:
-            all_products = soup.findAll(
-                'div', class_='product-card product-na ng-star-inserted')
+    soup = BeautifulSoup(driver.page_source, "html.parser")
 
-        for product in all_products:
-            very_internal = []
-            try:
-                name = product.find(
-                'p', class_='pc-title ant-typography ant-typography-ellipsis ant-typography-ellipsis-multiple-line').text.strip()
-            except:
-                name = '0'
-            try:
-                price = product.find(
-                'div', class_='pc-cost ng-star-inserted').text.strip()
-            except:
-                price = '0'
-            try:
-                image = product.find('img', class_='pc-img').attrs['src'].strip()
-            except:
-                image = '0'
-            try:
-                link = base_url + product.find('a', class_='cursor-pointer').attrs['href'].strip()
-            except:
-                link = '0'
-            try:
-                coin = product.find(
-                'div', class_='pc-rewards ng-star-inserted').text.strip()
-            except:
-                coin = '0'            
-            try:
-                orignal_price = product.find(
-                    'div', class_='pc-p-old ng-star-inserted').text.strip()
-            except:
-                orignal_price = '0'
-            try:
-                discount_percentage = product.find(
-                    'div', class_='pc-tag ng-star-inserted').text.strip()
-            except:
-                discount_percentage = '0'
-            try:
-                product_avalible = product.find(
-                    'button', class_='pc-add-btn ant-btn ant-btn-dangerous ant-btn-block ng-star-inserted').text.strip()
-            except:
-                product_avalible = 'Out of Stock'
+    #all_links = soup.select_one('ecp-app-side-panel aside ecp-category nav ul')
+                    #class_='table table-striped table-bordered')
+    all_links_raw = soup.select('ecp-category nav ul li a')
+    all_links = []
+    all_link_categories = []
+    loop_counter = 1
+    for link in all_links_raw:
+        full_link = base_url + link.attrs['href']
+        category_name = link.text
+        all_links.append(full_link)
+        all_link_categories.append(category_name)
+        print(full_link)
+        loop_counter += 1
 
-            very_internal.append(name)
-            very_internal.append(price_value_filter(price))
-            very_internal.append(image)
-            very_internal.append(link)
-            very_internal.append(coin_value_filter(coin))
-            very_internal.append(price_value_filter(orignal_price))
-            very_internal.append(percentage_value_filter(discount_percentage))
-            very_internal.append(product_in_stock(product_avalible))
-            very_internal.append(location)
+    # save cookies
+    #pickle.dump( driver.get_cookies() , open("cookies.pkl","wb"))
 
-            # print(name)
-            # print(price_value_filter(price))
-            # print(image)
-            # print(link)
-            # print(coin_value_filter(coin))
-            # print(price_value_filter(orignal_price))
-            # print(percentage_value_filter(discount_percentage))
-            # print(product_in_stock(product_avalible))
-            # print(location)
-            # print('-----------------------------------------------')
+    # driver.get('https://www.airliftexpress.com/product-category/promotions?page=50')
+    #driver.get('https://www.airliftexpress.com/product-category/promotions')
+    all_item_list = []
+    start_time=time.time()
 
-            # db_operation.insert_data_into_airlift_table(very_internal)
+    for link in all_links:
+        driver.get(link)
+        driver.execute_script("document.body.style.zoom='zoom 75%'")
+        time.sleep(5)
+        db_operation = database_operations()
+        SCROLL_PAUSE_TIME = 1
+        finalbreak = 0
+        last_product_1 = ''
+        last_product_2 = ''
+        external_list = []
+        while True:
+            soup = BeautifulSoup(driver.page_source, 'html.parser')
 
-            last_product_2 = name
-            external_list.append(very_internal)
+            location = soup.find('span', class_='dlb-location').text
 
-        #db_operation.insert_data_into_airlift_table(very_internal)
+            all_products = soup.findAll('div', class_='product-card ng-star-inserted')
+            if len(all_products) == 0:
+                all_products = soup.findAll(
+                    'div', class_='product-card product-na ng-star-inserted')
 
-        body = driver.find_element_by_css_selector('body')
-        body.click()
-        #body.send_keys(Keys.PAGE_DOWN)
-        body.send_keys(Keys.PAGE_DOWN)
-        time.sleep(SCROLL_PAUSE_TIME)
+            for product in all_products:
+                very_internal = []
+                try:
+                    name = product.find(
+                    'p', class_='pc-title ant-typography ant-typography-ellipsis ant-typography-ellipsis-multiple-line').text.strip()
+                except:
+                    name = '0'
+                try:
+                    price = product.find(
+                    'div', class_='pc-cost ng-star-inserted').text.strip()
+                except:
+                    price = '0'
+                try:
+                    image = product.find('img', class_='pc-img').attrs['src'].strip()
+                except:
+                    image = '0'
+                try:
+                    link = base_url + product.find('a', class_='cursor-pointer').attrs['href'].strip()
+                except:
+                    link = '0'
+                try:
+                    coin = product.find(
+                    'div', class_='pc-rewards ng-star-inserted').text.strip()
+                except:
+                    coin = '0'            
+                try:
+                    orignal_price = product.find(
+                        'div', class_='pc-p-old ng-star-inserted').text.strip()
+                except:
+                    orignal_price = '0'
+                try:
+                    discount_percentage = product.find(
+                        'div', class_='pc-tag ng-star-inserted').text.strip()
+                except:
+                    discount_percentage = '0'
+                try:
+                    product_avalible = product.find(
+                        'button', class_='pc-add-btn ant-btn ant-btn-dangerous ant-btn-block ng-star-inserted').text.strip()
+                except:
+                    product_avalible = 'Out of Stock'
 
-        if last_product_1 == last_product_2:
-            finalbreak = finalbreak+1
-            if finalbreak > 5:
-                break
-        else:
-            finalbreak = 0
-        last_product_1 = last_product_2
+                very_internal.append(name)
+                very_internal.append(price_value_filter(price))
+                very_internal.append(image)
+                very_internal.append(link)
+                very_internal.append(coin_value_filter(coin))
+                very_internal.append(price_value_filter(orignal_price))
+                very_internal.append(percentage_value_filter(discount_percentage))
+                very_internal.append(product_in_stock(product_avalible))
+                very_internal.append(location)
 
-    total_time_seconds = round(time.time()-start_time,0)
-    #external_list.append(total_time_seconds)
+                last_product_2 = name
+                external_list.append(very_internal)
+
+            #db_operation.insert_data_into_airlift_table(very_internal)
+
+            body = driver.find_element_by_css_selector('body')
+            body.click()
+            #body.send_keys(Keys.PAGE_DOWN)
+            body.send_keys(Keys.PAGE_DOWN)
+            time.sleep(SCROLL_PAUSE_TIME)
+
+            if last_product_1 == last_product_2:
+                finalbreak = finalbreak+1
+                if finalbreak > 5:
+                    break
+            else:
+                finalbreak = 0
+            last_product_1 = last_product_2
+
+        total_time_seconds = round(time.time()-start_time,0)
+        #external_list.append(total_time_seconds)
+        csv = csv_operations()
+        #stasts = csv.write_csvfile('airlift_product', external_list)
+        #status = file_operat.save_webpage_source('promotions.html', driver.page_source)
+        all_item_list.extend(external_list)
+
+    print(total_time_seconds)
     csv = csv_operations()
-    #stasts = csv.write_csvfile('airlift_product', external_list)
-    #status = file_operat.save_webpage_source('promotions.html', driver.page_source)
-    all_item_list.extend(external_list)
+    #stasts = csv.write_csvfile('airlift_product_full', all_item_list)
 
-print(total_time_seconds)
-csv = csv_operations()
-#stasts = csv.write_csvfile('airlift_product_full', all_item_list)
+    unique_list = unique_listoflist(all_item_list)
 
-unique_list = unique_listoflist(all_item_list)
+    stasts = csv.write_csvfile('airlift_product_full_unique', unique_list)
 
-stasts = csv.write_csvfile('airlift_product_full_unique', unique_list)
+    for link in unique_list:
+        insert_data_into_airlift_table(link)
+        time.sleep(0.01)
 
-for link in unique_list:
-    insert_data_into_airlift_table(link)
-    time.sleep(0.01)
-
-driver.close()
+    driver.close()
