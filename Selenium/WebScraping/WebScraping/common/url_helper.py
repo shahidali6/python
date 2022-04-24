16d9d711ba5a3f245956ca7564309518cc55f823
Shahid Saddique <shahidali6@gmail.com>
2022-04-24 23:02:09 +0500

    comments and formating added.

--- Selenium/WebScraping/WebScraping/common/url_helper.py
+++ Selenium/WebScraping/WebScraping/common/url_helper.py
@@ -1,8 +1,10 @@
 from urllib.parse import urlparse
 
-class URLHelper():
+# URL Helper class handle all URL related functions
+class url_helper:
+    # Function return the domain name from URL
     def domin_finder(self, url):
         try:
             return urlparse(url).hostname
         except Exception as ex:
-            return "Exception: "+str(ex)
+            return "Exception: " + str(ex)
