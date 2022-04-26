1d2e4fae6708370123800ad649d4600582bfe5d0
Shahid Saddique <shahidali6@gmail.com>
2022-04-26 23:05:46 +0500

    1. Selenium libraries added. 2. Remove categories from extract_categories and links function 3. New function added of extract product data.

--- Selenium/WebScraping/WebScraping/working/grocerapp.py
+++ Selenium/WebScraping/WebScraping/working/grocerapp.py
@@ -2,24 +2,28 @@ import datetime
 import time
 from bs4 import BeautifulSoup
 from bs4.element import SoupStrainer
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from urllib.parse import urlparse
+from selenium.webdriver.common.by import By
 
 class grocerapp_operation():
-    """The function extract the name and links of the grocerapp.pk website."""
+    """The function extract the categories and links of the grocerapp.pk website."""
     def extract_categories_links(self, base_url, html_to_extract):
         soup = BeautifulSoup(html_to_extract, "html.parser")
 
         #Selector used to get the list of categories
-        categories_raw = soup.select('main div div div div div div p a b')
+        #categories_raw = soup.select('main div div div div div div p a b')
         #Selector used to get the lsit of raw links
         links_raw = soup.select('main div div div div div div p a')
 
         #lists will use to return values
         refine_links = []
-        refine_categories = []
+        #refine_categories = []
 
         #Populate Categories name list
-        for category_text in categories_raw:
-            refine_categories.append( category_text.text)
+        #for category_text in categories_raw:
+        #    refine_categories.append( category_text.text)
 
         for link in links_raw:
             link_value = base_url+ link.attrs['href']
@@ -29,50 +33,83 @@ class grocerapp_operation():
                     refine_links.append(link_value)     
         
         print('No Links found') if len(refine_links) == 0 else print('Links found')
-        print('No Categories found') if len(refine_categories) == 0 else print('Categories found')
+        #print('No Categories found') if len(refine_categories) == 0 else print('Categories found')
 
         #Return the lists
-        return refine_links, refine_categories
+        return refine_links#, refine_categories
 
     """The function extract the name and links of the grocerapp.pk website."""
     def extract_product_data(self, base_url, html_to_extract):
-        soup = BeautifulSoup(html_to_extract, "html.parser")
+        base_url = "https://grocerapp.pk"
+        driver = webdriver.Chrome()
+        driver.maximize_window()
+        driver.get(base_url)
+        time.sleep(2)
+        grocer_obj = grocerapp_operation()
+        productListToReturn = []
 
-        time.sleep(5)
-        #Selector used to get the list of categories
-        main_product_container = soup.find_all('div', class_='muigrid-root muigrid-container')
-        for main in main_product_container:
-            #chcch = main.findchildren('div', class_='MuiPaper-root MuiCard-root jss1392 MuiPaper-elevation1 MuiPaper-rounded')
-            #product_car = main.find_all('div div div div div')
-            #product_car = main.find_all('div', class_='MuiGrid-root jss1372 MuiGrid-item MuiGrid-grid-xs-true')
-            product_car = main.find_all('div', class_='muigrid-root muigrid-container muigrid-direction-xs-column muigrid-wrap-xs-nowrap')
-            print('I am in!.')
-            
-        
-        product_car = main_product_container.findAll('div', class_='MuiGrid-root MuiGrid-container MuiGrid-direction-xs-column MuiGrid-justify-content-xs-space-between')
-        main_product_container1 = soup.findAll('div', class_='MuiGrid-root MuiGrid-container MuiGrid-direction-xs-column MuiGrid-justify-content-xs-space-between')
-        product_cards = soup.select('main div div div div div div div div')
-        product_cards = soup.select('main > div.jss552 > div.MuiGrid-root.MuiGrid-container > div:nth-child(1) > div > div > div.MuiGrid-root.jss1372.MuiGrid-item.MuiGrid-grid-xs-true > div')
-        #Selector used to get the lsit of raw links
-        links_raw = soup.select('main div div div div div div p a')
+        links = self.extract_categories_links(base_url, driver.page_source)
 
-        #lists will use to return values
-        refine_links = []
-        refine_categories = []
+        for link in links:
+            driver.get(link)
+            body = driver.find_element_by_css_selector('body')
+            loopTerminator = True
+            loopRetries = 0
+            bodyHeight = body.rect['height']
+            bodyHeightCheck = ''
 
-        #Populate Categories name list
-        for category_text in categories_raw:
-            refine_categories.append( category_text.text)
+            while loopTerminator:
+                #body.send_keys(Keys.PAGE_DOWN)
+                body.send_keys(Keys.END)
+                time.sleep(1)
+                bodyHeightCheck = body.rect['height']
+                if bodyHeight == bodyHeightCheck:
+                    if loopRetries > 10:
+                        loopTerminator = False
+                    loopRetries += 1
+                else:
+                    bodyHeight = bodyHeightCheck
+                    loopRetries = 0
+    
+            products_xpath = '/html/body/div[1]/div/main/div[1]/div[3]/div/div/div/div[2]/div'
+            main_content = driver.find_elements(by=By.XPATH, value=products_xpath)
+            #main_content = driver.find_elements_by_xpath(products_xpath)
+            for item in main_content:
+                singleListProduct =[]
+                name = category = price = image = link = coin = orignal_price = discount_percentage = units = product_avalible = location = source = ''
 
-        for link in links_raw:
-            link_value = base_url+ link.attrs['href']
-            link_text = link.text
-            for check_category in refine_categories:
-                if check_category == link_text:
-                    refine_links.append(link_value)     
-        
-        print('No Links found') if len(refine_links) == 0 else print('Links found')
-        print('No Categories found') if len(refine_categories) == 0 else print('Categories found')
+                category = driver.find_element(by=By.XPATH, value='/html/body/div[1]/div/main/div[1]/div[1]/h1').text
+                location = driver.find_element(by=By.XPATH, value='/html/body/div[1]/div/main/header/div/div[1]/div/div[1]/div/div[2]/div/div/p').text
+                #source = url.domin_finder(driver.current_url)
+                source = urlparse(driver.current_url).hostname
+                link = item.find_element(by=By.TAG_NAME, value='a').get_attribute('href')
+                image = item.find_element(by=By.TAG_NAME, value='img').get_attribute('src')
+                listProduct = item.text.split('\n')
+                if len(listProduct) == 6:
+                    name = listProduct[0]
+                    units = listProduct[1]
+                    price = listProduct[2]
+                    orignal_price = listProduct[3]
+                    discount_percentage = listProduct[4]
+                    product_avalible = listProduct[5]
 
-        #Return the lists
-        return refine_links, refine_categories
+                if len(listProduct) == 4:
+                    name = listProduct[0]
+                    units = listProduct[1]
+                    price = listProduct[2]
+                    product_avalible = listProduct[3]
+
+                singleListProduct.append(name,category, price, image, link, orignal_price, discount_percentage, units, product_avalible, location, source)
+                print(name)
+                print(category)
+                print(price)
+                print(image)
+                print(link)
+                print(orignal_price)
+                print(discount_percentage)
+                print(units)
+                print(product_avalible)
+                print(location)
+                print(source)
+                print('-----------------------------------------')
+            productListToReturn.append(singleListProduct)
